276b65b9ef37eead2ec168a5dbd7fc7be213bcc4
odoocker <yhaelopez@gmail.com>
2023-10-10 04:29:07 +0000

    aws setup working

--- odoo/odoorc.sh
+++ odoo/odoorc.sh
@@ -5,9 +5,6 @@ set -e
 # Define the path to the example configuration file
 TEMPLATE_CONF="odoo.conf"
 
-# Copy the example conf to the destination to start replacing the variables
-cp "$TEMPLATE_CONF" "$ODOO_RC"
-
 # First pass: Evaluate any nested variables within .env file and export them
 while IFS='=' read -r key value || [[ -n $key ]]; do
     # Skip comments and empty lines
@@ -22,6 +19,10 @@ while IFS='=' read -r key value || [[ -n $key ]]; do
     
     export "$key=$value"
 done < .env
+
+# Copy the example conf to the destination to start replacing the variables
+echo "$TEMPLATE_CONF" "$ODOO_RC"
+cp "$TEMPLATE_CONF" "$ODOO_RC"
 
 # Second pass: Replace the variables in $ODOO_RC
 while IFS='=' read -r key value || [[ -n $key ]]; do
