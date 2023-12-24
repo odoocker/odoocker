77edc03b9184ae13a0c15f14039abbc808915ab5
yhaelopez <yhaelopez@gmail.com>
2023-12-24 12:22:16 -0700

    refactored enterprise + third party addons clonning process

--- odoo/clone-enterprise.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [ -n "$ENTERPRISE_USER" ] && [ -n "$ENTERPRISE_ACCESS_TOKEN" ]; then \
-    git clone https://${ENTERPRISE_USER}:${ENTERPRISE_ACCESS_TOKEN}@github.com/odoo/enterprise.git ${ENTERPRISE_ADDONS} --depth 1 --branch ${ODOO_TAG} --single-branch --no-tags
-fi
